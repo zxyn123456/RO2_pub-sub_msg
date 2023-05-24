343c2d7bc3c7cc6136bf5368d59909b2d46dc964
zxyn123456 <90199940+zxyn123456@users.noreply.github.com>
2023-05-24 17:40:38 +0800

    示例代码
    包含怎么订阅topic，收发topic下的消息，cmakelists和xml写法
    在ROS2的foxy测试OK

---
+++ control_pub/src/subscriber_member_function.cpp
@@ -0,0 +1,49 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+// #include "nav_msgs/Odometry.h"
+   
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+  MinimalSubscriber()
+  : Node("minimal_subscriber")
+  {
+    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/carla/ego_vehicle/odometry", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->pose.pose.position.x);
+  }
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
