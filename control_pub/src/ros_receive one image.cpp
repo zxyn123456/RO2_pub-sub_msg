976eaccfcff2731207f6a3625c9505df6ece1409
zxyn123456 <90199940+zxyn123456@users.noreply.github.com>
2023-06-26 16:44:23 +0800

    增加this->

--- control_pub/src/ros_receive one image.cpp
+++ control_pub/src/ros_receive one image.cpp
@@ -1,29 +1,53 @@
-#include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include <chrono>
+#include "ipm.h"
+//#include <opencv2/opencv.hpp>
+//#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.hpp>
 
-class MyNode : public rclcpp::Node {
+
+class IPM : public rclcpp::Node
+{
 public:
-  MyNode() : Node("my_node") {
-    subscription_ = create_subscription<sensor_msgs::msg::Image>(
-        "image_topic", 
-        10, 
-        std::bind(&MyNode::image_callback, this, std::placeholders::_1));
+  IPM()
+  : Node("ipm_compose")
+  {
+    subscription_front = this->create_subscription<sensor_msgs::msg::Image>(
+      "/carla/ego_vehicle/rgb_front/image", 10, std::bind(&IPM::topic_callback, this, std::placeholders::_1));
+/*
+    subscription_left = this->create_subscription<sensor_msgs::msg::Image>(
+      "/carla/ego_vehicle/rgb_left/image", 10, std::bind(&IPM::topic_callback, this, std::placeholders::_1,"left"));
+
+    subscription_rear = this->create_subscription<sensor_msgs::msg::Image>(
+      "/carla/ego_vehicle/rgb_rear/image", 10, std::bind(&IPM::topic_callback, this, std::placeholders::_1,"rear"));
+
+    subscription_right = this->create_subscription<sensor_msgs::msg::Image>(
+      "/carla/ego_vehicle/rgb_right/image", 10, std::bind(&IPM::topic_callback, this, std::placeholders::_1,"right"));
+
+    front_image = cv::Mat();
+    left_image = cv::Mat();
+    rear_image = cv::Mat();
+    right_image = cv::Mat();   */
   }
 
 private:
-  void image_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
-    RCLCPP_INFO(get_logger(), "Received image message");
+ // void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg, const std::string& camera)
+  void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+  {
+        RCLCPP_INFO(get_logger(), "Received image message");
   }
 
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_front;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_left;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_rear;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_right;
 };
 
-int main(int argc, char ** argv) {
+int main(int argc, char * argv[])
+{
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MyNode>());
+  rclcpp::spin(std::make_shared<IPM>());
   rclcpp::shutdown();
   return 0;
 }
-
-
-// cmakelists和xml中需要添加sensor_msgs的依赖
